84994e27672bcb6e860d646426402d44687a8a50
107061217 <1067pr@gamil.com>
2020-03-17 20:12:30 +0800

    first commit

--- hw1.py
+++ hw1.py
@@ -59,69 +59,60 @@ target_data = list(filter(lambda item: (item['station_id'] == 'C0A880' or item['
 
 # Print result
 
-n0 = 0
-n1 = 0
-n2 = 0
-n3 = 0
-n4 = 0
-sum = 0.0
-SP_A880 = 0.0
-SP_F9A0 = 0.0
-SP_G640 = 0.0
-SP_R190 = 0.0
-SP_X260 = 0.0
+HUMD_C0A880 = 0.0
+HUMD_C0F9A0 = 0.0
+HUMD_C0G640 = 0.0
+HUMD_C0R190 = 0.0
+HUMD_C0X260 = 0.0
 
 for i in range(len(target_data)):
    if (target_data[i]['station_id'] == 'C0A880') :
       if (target_data[i]['HUMD'] == '-99.000' or target_data[i]['HUMD'] == '-999.000') :
-         SP_A880 = SP_A880
+         HUMD_C0A880 = HUMD_C0A880
       else :
-         SP_A880 = SP_A880 + float(target_data[i]['HUMD'])
+         HUMD_C0A880 = HUMD_C0A880 + float(target_data[i]['HUMD'])
    elif (target_data[i]['station_id'] == 'C0F9A0') :
       if (target_data[i]['HUMD'] == '-99.000' or target_data[i]['HUMD'] == '-999.000') :
-         SP_F9A0 = SP_F9A0
+         HUMD_C0F9A0 = HUMD_C0F9A0
       else :
-         SP_F9A0 = SP_F9A0 + float(target_data[i]['HUMD'])
+         HUMD_C0F9A0 = HUMD_C0F9A0 + float(target_data[i]['HUMD'])
    elif (target_data[i]['station_id'] == 'C0G640') :
       if (target_data[i]['HUMD'] == '-99.000' or target_data[i]['HUMD'] == '-999.000') :
-         SP_G640 = SP_G640
+         HUMD_C0G640 = HUMD_C0G640
       else :
-         SP_G640 = SP_G640 + float(target_data[i]['HUMD'])
+         HUMD_C0G640 = HUMD_C0G640 + float(target_data[i]['HUMD'])
    elif (target_data[i]['station_id'] == 'C0R190') :
       if (target_data[i]['HUMD'] == '-99.000' or target_data[i]['HUMD'] == '-999.000') :
-         SP_R190 = SP_R190
+         HUMD_C0R190 = HUMD_C0R190
       else :
-         SP_R190 = SP_R190 + float(target_data[i]['HUMD'])
+         HUMD_C0R190 = HUMD_C0R190 + float(target_data[i]['HUMD'])
    elif (target_data[i]['station_id'] == 'C0X260') :
       if (target_data[i]['HUMD'] == '-99.000' or target_data[i]['HUMD'] == '-999.000') :
-         SP_X260 = SP_X260
+         HUMD_C0X260 = HUMD_C0X260
       else :
-         SP_X260 = SP_X260 + float(target_data[i]['HUMD'])
+         HUMD_C0X260 = HUMD_C0X260 + float(target_data[i]['HUMD'])
 
-if (SP_A880 == 0) :
-   val_A880 = str(None)
+if (HUMD_C0A880 == 0) :
+   val_C0A880 = str(None)
 else :
-   val_A880 = str(format(SP_A880, '.2f'))
-if (SP_F9A0 == 0) :
-   val_F9A0 = str(None)
+   val_C0A880 = str(format(HUMD_C0A880, '.2f'))
+if (HUMD_C0F9A0 == 0) :
+   val_C0F9A0 = str(None)
 else :
-   val_F9A0 = str(format(SP_F9A0, '.2f'))
-if (SP_G640 == 0) :
-   val_G640 = str(None)
+   val_C0F9A0 = str(format(HUMD_C0F9A0, '.2f'))
+if (HUMD_C0G640 == 0) :
+   val_C0G640 = str(None)
 else :
-   val_G640 = str(format(SP_G640, '.2f'))
-if (SP_R190 == 0) :
-   val_R190 = str(None)
+   val_C0G640 = str(format(HUMD_C0G640, '.2f'))
+if (HUMD_C0R190 == 0) :
+   val_C0R190 = str(None)
 else :
-   val_R190 = str(format(SP_R190, '.2f'))
-if (SP_X260 == 0) :
-   val_X260 = str(None)
+   val_C0R190 = str(format(HUMD_C0R190, '.2f'))
+if (HUMD_C0X260 == 0) :
+   val_C0X260 = str(None)
 else :
-   val_X260 = str(format(SP_X260, '.2f'))
+   val_C0X260 = str(format(HUMD_C0X260, '.2f'))
 
 
-print('''[['C0A880',''', val_A880 + '],', '''['C0F9A0',''', val_F9A0 + '],', '''['C0G640',''', val_G640 + '],', '''['C0R190',''', val_R190 + '],', '''['C0X260',''', val_X260 + ']]')   
+print('''[['C0A880',''', val_C0A880 + '],', '''['C0F9A0',''', val_C0F9A0 + '],', '''['C0G640',''', val_C0G640 + '],', '''['C0R190',''', val_C0R190 + '],', '''['C0X260',''', val_C0X260 + ']]')   
 
-#print("['C0R190', ", target_data[2]['HUMD'], "]")
-
-#========================================
